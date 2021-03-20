5ae52450514854a89f4dff91ff3fcae53583340b
dudikpal <dudikpal@gmail.com>
2021-03-20 20:54:37 +0100

    Regex MatchingAnythingButANewline

---
+++ src/main/java/regex/matchinganythingbutanewline/MatchingAnythingButANewline.java
@@ -0,0 +1,18 @@
+package regex.matchinganythingbutanewline;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchingAnythingButANewline {
+    
+    
+    public boolean check(String pattern){
+        
+        String testString = "123.456.abc.def";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(testString);
+        
+        return m.matches();
+        
+    }
+}
---
+++ src/test/java/regex/matchinganythingbutanewline/MatchingAnythingButANewlineTest.java
@@ -0,0 +1,15 @@
+package regex.matchinganythingbutanewline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchingAnythingButANewlineTest {
+    
+    @Test
+    void check() {
+        
+        assertEquals(true, new MatchingAnythingButANewline().check("(...\\.){3}..."));
+        
+    }
+}
