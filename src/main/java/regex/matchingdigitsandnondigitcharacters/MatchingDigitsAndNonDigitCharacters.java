75a8e1140cf49c6a576747cc27147a82c2e972a5
dudikpal <dudikpal@gmail.com>
2021-03-20 21:06:27 +0100

    Regex MatchingDigitsAndNonDigitCharacters

---
+++ src/main/java/regex/matchingdigitsandnondigitcharacters/MatchingDigitsAndNonDigitCharacters.java
@@ -0,0 +1,19 @@
+package regex.matchingdigitsandnondigitcharacters;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchingDigitsAndNonDigitCharacters {
+    
+    
+    
+    public boolean check(String pattern){
+        
+        String testString = "06-11-2015";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(testString);
+        
+        return m.matches();
+        
+    }
+}
---
+++ src/test/java/regex/matchingdigitsandnondigitcharacters/MatchingDigitsAndNonDigitCharactersTest.java
@@ -0,0 +1,15 @@
+package regex.matchingdigitsandnondigitcharacters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchingDigitsAndNonDigitCharactersTest {
+    
+    @Test
+    void check() {
+        
+        assertEquals(true, new MatchingDigitsAndNonDigitCharacters().check("(\\d\\d\\D){2}\\d{4}"));
+        
+    }
+}
