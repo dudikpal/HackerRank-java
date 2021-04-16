028c758607181d5e0cb1c02330455896431c0171
dudikpal <dudikpal@gmail.com>
2021-04-16 23:20:18 +0200

    #1: Java - Easy - Advanced - Varargs Simple Addition done

---
+++ src/main/java/_java/easy/varargssimpleaddition/VarargsSimpleAddition.java
@@ -0,0 +1,39 @@
+package _java.easy.varargssimpleaddition;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class VarargsSimpleAddition {
+
+    public String solution(String input) {
+    
+        String[] lines = input.split("\n");
+        StringBuilder sb = new StringBuilder();
+        String separator = "\n";
+        sb.append(add(lines[0], lines[1]));
+        sb.append(separator);
+        sb.append(add(lines[0], lines[1], lines[2]));
+        sb.append(separator);
+        sb.append(add(lines[0], lines[1], lines[2], lines[3], lines[4]));
+        sb.append(separator);
+        sb.append(add(lines[0], lines[1], lines[2], lines[3], lines[4], lines[5]));
+        return sb.toString();
+    }
+    
+    private String add(String... numbers) {
+        StringBuilder sb = new StringBuilder();
+        String separator = "";
+        int sum = 0;
+        for (String number : numbers) {
+            sb.append(separator);
+            sb.append(number);
+            separator = "+";
+            sum += Integer.parseInt(number);
+        }
+        sb.append("=");
+        sb.append(sum);
+        return sb.toString();
+    }
+}
---
+++ src/test/java/_java/easy/varargssimpleaddition/VarargsSimpleAdditionTest.java
@@ -0,0 +1,26 @@
+package _java.easy.varargssimpleaddition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VarargsSimpleAdditionTest {
+    
+    @Test
+    void test() {
+        String input = "1\n" +
+                           "2\n" +
+                           "3\n" +
+                           "4\n" +
+                           "5\n" +
+                           "6";
+        
+        String expected = "1+2=3\n" +
+                              "1+2+3=6\n" +
+                              "1+2+3+4+5=15\n" +
+                              "1+2+3+4+5+6=21";
+        
+        assertEquals(expected, new VarargsSimpleAddition().solution(input));
+    }
+    
+}
